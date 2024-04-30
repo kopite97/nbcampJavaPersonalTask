49a6885738cbe125c51f78a6a78ed7ee9c985280
Robbie <robbie331@>
2024-04-30 16:29:38 +0900

    level2 요구ì사항7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,33 +12,54 @@ public class App {
         Calculator calculator = new Calculator();
 
         do {
-            System.out.print("Input first Number: ");
-            int firstNumber = sc.nextInt();
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            System.out.print("Input second Number: ");
-            int secondNumber = sc.nextInt();
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-
-            System.out.print("Input Operator: ");
-            String operator = sc.next();
-            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            System.out.println("elementary arithmetic / area of a circle");
+            System.out.println("Input \"e\"/\"c\"");
+            String checkOper = sc.nextLine();
             Double result = 0.0;
-            try {
-                result = calculator.Calculate(firstNumber, secondNumber, operator);
-            } catch (CalculateException e) {
-                System.out.println(e.getMessage());
+
+            if (checkOper.equals("e")) {
+                // 사칙연산 수행
+
+                System.out.print("Input first Number: ");
+                int firstNumber = sc.nextInt();
+                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                System.out.print("Input second Number: ");
+                int secondNumber = sc.nextInt();
+                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+
+                System.out.print("Input Operator: ");
+                String operator = sc.next();
+                // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+
+                try {
+                    result = calculator.Calculate(firstNumber, secondNumber, operator);
+                } catch (CalculateException e) {
+                    System.out.println(e.getMessage());
+                    continue;
+                }
+                System.out.println("Result: " + result);
+            } else if (checkOper.equals("c")) {
+                // 원의 넓이 구하기 수행
+                System.out.println("Input radius");
+                double radius = sc.nextDouble();
+                result = calculator.CalculateCircle(radius);
+                System.out.println("Area of circle : " + result);
+            } else {
+                System.out.println("Wrong input");
                 continue;
             }
 
-            System.out.println("Result: " + result);
 
-            System.out.println("Do you want to Exit?? (Input \"exit\" / \"inquiry\" / \"remove\")");
+            System.out.println("Do you want to exit?? (Input \"exit\" / \"(inquiry/cinquiry)\" / \"remove\")");
             checkExit = sc.next();
 
             if (checkExit.equals("exit"))
                 break;
             else if (checkExit.equals("inquiry")) {
                 calculator.InquiryResults();
+            }
+            else if (checkExit.equals("cinquiry")) {
+                calculator.InquiryCircleResults();
             }
             else if (checkExit.equals("remove")) {
                 if (calculator.RemoveResult()) {
@@ -47,5 +68,9 @@ public class App {
             }
 
         } while (true);
+    }
+
+    public void ElementaryArithmetic() {
+
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,21 +7,48 @@ public class Calculator {
 
     private Queue<Double> results;
 
+    // circleResults를 다시 선언할 일도 없고 중복될 일도 없으니 static을 굳이 쓰지 않았습니다.
+    // 또한 final을 써도 정상적으로 작동하겠지만 circleResults의 값을 실수로라도 바꿀 일이 일단은 없기 때문에 사용하지 않았습니다.
+    private Queue<Double> circleResults;
+
+    public Queue<Double> getCircleResults() {
+        return circleResults;
+    }
+
+    public void setCircleResults(Queue<Double> circleResults) {
+        this.circleResults = circleResults;
+    }
+
     public Queue<Double> getResults() {
         return results;
     }
+
     public void setResults(LinkedList<Double> results) {
         this.results = results;
     }
 
-    public boolean RemoveResult()
-    {
-        return results.poll()!= null;
+    public boolean RemoveResult() {
+        return results.poll() != null;
     }
 
     public void InquiryResults() {
+        if (results.size() <= 0) {
+            System.out.println("No results found");
+        }
+
         System.out.print("Results : ");
         for (var number : results) {
+            System.out.print(number + " ");
+        }
+        System.out.println("\n-----");
+    }
+
+    public void InquiryCircleResults() {
+        if (circleResults.size() <= 0) {
+            System.out.println("No circle results found");
+        }
+        System.out.print("Circle Results : ");
+        for (var number : circleResults) {
             System.out.print(number + " ");
         }
         System.out.println("\n-----");
@@ -32,16 +59,16 @@ public class Calculator {
 
         switch (operator) {
             case "+":
-                result = (double)(firstNumber+secondNumber);
+                result = (double) (firstNumber + secondNumber);
                 break;
             case "-":
-                result = (double)(firstNumber-secondNumber);
+                result = (double) (firstNumber - secondNumber);
                 break;
             case "*":
-                result = (double)(firstNumber*secondNumber);
+                result = (double) (firstNumber * secondNumber);
                 break;
             case "/":
-                result = (double)(firstNumber/secondNumber);
+                result = (double) (firstNumber / secondNumber);
                 break;
             default:
                 throw new CalculateException(operator);
@@ -50,7 +77,16 @@ public class Calculator {
         return result;
     }
 
+    public Double CalculateCircle(double radius) {
+        Double result = Math.PI * Math.pow(radius, 2);
+
+        circleResults.add(result);
+
+        return result;
+    }
+
     public Calculator() {
         results = new LinkedList<>();
+        circleResults = new LinkedList<>();
     }
 }
