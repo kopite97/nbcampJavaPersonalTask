def624133a897369b5a717c7d06d96a9ee0dd6ba
Robbie <robbie331@>
2024-04-30 14:05:51 +0900

    문ë변수 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,12 +6,17 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
+
+
+        System.out.print("Input first Number: ");
+        int firstNumber = sc.nextInt();
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.print("두 번째 숫자를 입력하세요: ");
+        System.out.print("Input second Number: ");
+        int secondNumber = sc.nextInt();
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
+        System.out.print("Input Operator: ");
+        String operator = sc.next();
         // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
     }
 }
